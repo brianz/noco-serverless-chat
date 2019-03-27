5e5910b8268858de4d7a5b4267c6747f13553719
Brian Zambrano <brianz@gmail.com>
2019-03-27 16:08:21 -0600

    Add connect/disconnect/help code and setup for broadcast exercise
    Why
    ---
    
    - In this step, the goal is just to take an incoming message and
    broadcast it out to the other connected users.
    
    This change addresses the need by
    ---------------------------------
    
    - Setting up all of the helper functions to do the dirty work
    - Taking care of recording who is logged in vis the `connect` and
    `disconnect` implementations.
    
    Next Steps
    ----------
    
    - Have people implement the details in the `default` handler. The steps
    are outlined in the comments in that funciton.

--- serverless/handler.py
+++ serverless/handler.py
@@ -4,25 +4,45 @@ import boto3
 import sys
 
 from noco import aws
+from noco.helpers import safe_dumps
 
 
 def default(event, context):
     """Default handler for websocket messages"""
-    print(event)
+    message = event.get('body', '')
 
-    # Steps to send yourself a message
+    if not message.strip():
+        return {
+            'statusCode': 200,
+        }
+
+    if message.startswith('/'):
+        return _handle_slash(message, event)
+
+    # Steps to broadcast messages to all connected users. All of the helper functions you need for
+    # this exist in the `noco/aws.py` file.
     #
-    # 1. Get the message from the event above. Fire up a websocket connection and type some
-    #    stuff...see where it shows up in the event by looking at the CloudWatch logs
-    # 2. After you know how to get the arriving message, use one of the helper functions
-    #    below to send yourself back the same message.
+    # 1. Get the user identified by the incoming connection_id from DynamoDB.
+    # 2. After you have the user record, save the incoming message to DynamoDB
+    # 3. Now that the message is saved to DynamoDB, broadcast that message out to everyone else who
+    #    is connected.
+
+    # Get the user from dynamodb
+
+    # Save the message to dynamodb
+
+    # broadcast the message to all connected users
 
     return {
         'statusCode': 200,
+        'body': safe_dumps(message),
     }
 
 
 def connect(event, context):
+    connection_id = _get_connection_id(event)
+    aws.set_connection_id(connection_id)
+
     return {
         'statusCode': 200,
         'body': 'connect',
@@ -30,9 +50,64 @@ def connect(event, context):
 
 
 def disconnect(event, context):
+    connection_id = _get_connection_id(event)
+
+    user = aws.get_user(connection_id)
+    channel_name = user.get('channel_name', 'general')
+    aws.delete_connection_id(connection_id, channel_name)
+
     return {
         'statusCode': 200,
         'body': 'disconnect',
+    }
+
+
+def _handle_slash(message, event):
+    if message.startswith('/name '):
+        return _set_name(message, event)
+
+    if message.startswith('/channel '):
+        return _set_channel(message, event)
+
+    return _help(message, event)
+
+
+def _help(event):
+    message = "Valid commands: /help, /name [NAME], /channel [CHAN_NAME]"
+    _send_message_to_client(event, message)
+    return {
+        'statusCode': 200,
+        'body': 'help',
+    }
+
+
+def _set_name(message, event):
+    name = message.split('/name')[-1]
+    connection_id = _get_connection_id(event)
+    aws.save_username(connection_id, name.strip())
+
+    _send_message_to_client(event, 'Set username to {}'.format(name.strip()))
+
+    return {
+        'statusCode': 200,
+        'body': 'name',
+    }
+
+
+def _set_channel(message, event):
+    channel_name = message.split('/channel')[-1]
+    channel_name = channel_name.strip('# ')
+
+    connection_id = _get_connection_id(event)
+
+    aws.update_channel_name(connection_id, channel_name)
+    aws.set_connection_id(connection_id, channel=channel_name)
+
+    _send_message_to_client(event, 'Changed to #{}'.format(channel_name))
+
+    return {
+        'statusCode': 200,
+        'body': 'name',
     }
 
 
